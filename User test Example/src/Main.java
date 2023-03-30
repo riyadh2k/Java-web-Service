ca05277bee485683ff1a913ad33ffedec6f9c26c
Syed Shabenur Hassan <112406985+riyadh2k@users.noreply.github.com>
2023-03-30 11:34:02 +0200

    Delete User test Example directory

--- User test Example/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-
-        Rektangel rek = new Rektangel(10,5);
-        System.out.println("Width: "+ rek.width+"Height: "+rek.height);
-    }
-}
